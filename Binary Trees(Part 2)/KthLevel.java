7e58f70eee936be972413cde4c9de9d379271feb
Shashank Zarikar <zarikarshashank2005@gmail.com>
2026-05-25 15:27:30 +0530

    Implemented code to print Kth Level of Binary Tree

---
+++ Binary Trees(Part 2)/KthLevel.java
@@ -0,0 +1,44 @@
+public class KthLevel{
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static void kthLevel(Node root , int k ,int level){
+        if(root==null) return;
+        if(level==k){
+            System.out.print(root.data+" ");
+            return;
+        } 
+        kthLevel(root.left,k,level+1);
+        kthLevel(root.right,k,level+1);
+    }
+    public static void main(String args[]){
+
+        /*
+            1
+          /   \
+         2     3
+        / \   / \
+       4   5 6   7
+       */
+
+         Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        kthLevel(root,3,1);
+
+    }
+}
