7cbb97878a70c28f4273adbbdbc64c06018929df
Shashank Zarikar <zarikarshashank2005@gmail.com>
2026-05-26 16:36:43 +0530

    Implemented code to find Kth Ancestor of a node

---
+++ Binary Trees(Part 2)/KthAncestor.java
@@ -0,0 +1,55 @@
+public class KthAncestor {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static int  kthancestor(Node root,int node, int k){
+
+        if(root==null) return -1;
+
+        if(root.data==node) return 0;
+        
+        int left=kthancestor(root.left,node,k);
+        int right=kthancestor(root.right,node,k);
+
+        if(left==-1 && right==-1) return -1;
+        
+        int max=Math.max(left,right);
+
+        if(max+1==k) System.out.println(root.data);
+
+        return max+1;
+    }
+
+     public static void main(String args[]){
+
+        /*
+            1
+          /   \
+         2     3
+        / \   / \
+       4   5 6   7
+       */
+         Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        
+        kthancestor(root,4,1);
+        
+    }
+    
+}
