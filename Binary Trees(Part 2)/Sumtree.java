7fbbaa37a062cad2f04cc698bc0a2d42331ceebe
Shashank Zarikar <zarikarshashank2005@gmail.com>
2026-05-27 22:36:49 +0530

    Implemented code to transform a tree onto SumTree

---
+++ Binary Trees(Part 2)/Sumtree.java
@@ -0,0 +1,54 @@
+public class Sumtree {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    public static int sumtree(Node root){
+        if(root==null) return 0;
+        int left=sumtree(root.left);
+        int right=sumtree(root.right);
+        int data=root.data;
+        root.data=left+right;
+        return root.data+data;
+    }
+
+    public static void preorder(Node root){
+        if(root==null) return;
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String args[]){
+         /*
+            1
+          /   \
+         2     3
+        / \   / \
+       4   5 6   7
+       */
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        sumtree(root);
+        preorder(root);
+        
+
+    }
+}
